35870fc69cd183a116f43a22a0b6b21492b4024f
nejishow <ashrafmuhamad2014@gmail.com>
2022-07-30 16:04:01 +0300

    logo

--- src/components/UI/footer.vue
+++ src/components/UI/footer.vue
@@ -6,9 +6,9 @@
           <p class="mb-0">Copyright 2022 © Lotus.</p>
         </div>
         <div class="col-md-6">
-          <p class="pull-right mb-0">
+          <!-- <p class="pull-right mb-0">
             Hand crafted & made with <i class="fa fa-heart"></i>
-          </p>
+          </p> -->
         </div>
       </div>
     </div>
